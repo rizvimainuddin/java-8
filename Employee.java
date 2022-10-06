0aaeb4c06c8e6c59b539ae98102567062a7cc28e
Mainuddin Rizvi <mainuddin.rizvi@tothenew.com>
2022-10-06 16:01:52 +0530

    java 8 first version

---
+++ Employee.java
@@ -0,0 +1,11 @@
+class Employee{
+    String fullName;
+    Long salary;
+    String city;
+
+    public Employee(String fullName, Long salary, String city) {
+        this.fullName = fullName;
+        this.salary = salary;
+        this.city = city;
+    }
+}
---
+++ Operation.java
@@ -0,0 +1,11 @@
+public class Operation {
+    public int addition(int value1, int value2) {
+        return value1+value2;
+    }
+    public int subtract (int value1, int value2) {
+        return value1-value2;
+    }
+    public int multiplication(int value1, int value2) {
+        return value1 * value2;
+    }
+}
